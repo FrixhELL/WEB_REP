ec1e0bc3ada513e7159031f55b34b4f29f72a28d
FrixhELL <sarapovrodion@gmail.com>
2024-03-03 18:03:29 +0200

    Add counting of studying days

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -26,5 +26,12 @@ $last_month = 1187.23;
 $this_month = 1089.98;
 echo "\n";
 echo "Разница между расходами в прошлом месяце и в этом месяце: ", $last_month-$this_month;
+//3
+$num_languages = 4;
+$months = 11;
+$days = $months*16;
+$days_per_language = $days/$num_languages;
+echo "\n";
+echo "Дней в среднем у нее ушло на изучение каждого языка: ", $days_per_language;
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
