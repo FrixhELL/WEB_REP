a33a26ed9728f0624a1bc42382cc3c725fcbac14
FrixhELL <sarapovrodion@gmail.com>
2024-03-03 17:57:17 +0200

    Add numeric variables

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -4,9 +4,27 @@ $very_bad_unclear_name = "15 chicken wings";
 
 
 // Write your code here:
+//1
 $order = &$very_bad_unclear_name;
 $order .= " with french fries";
+//2
+$a = 1234;
+echo $a;
 
+$b = 256;
+echo "\n";
+echo $b;
 
+$c = 23.6;
+echo "\n";
+echo $c;
+
+echo "\n";
+echo 10+2;
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo "\n";
+echo "Разница между расходами в прошлом месяце и в этом месяце: ", $last_month-$this_month;
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
